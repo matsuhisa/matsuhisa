cd8fd036feb7019296080a993feadd7020c70b01
matsuhisa <matsuhisa+github@gmail.com>
2016-10-23 18:16:57 +0900

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,46 +1,18 @@
-###
-# Page options, layouts, aliases and proxies
-###
+Time.zone = "Tokyo"
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+activate :blog do |blog|
+  blog.per_page          = 1
+  blog.permalink         = "/articles/{title}"
+  blog.default_extension = ".md"
+  blog.sources           = 'articles/{title}.html'
+end
+activate :directory_indexes
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
 
 set :markdown_engine, :redcarpet
---
+++ source/articles/hello_world.html.md
@@ -0,0 +1,7 @@
+---
+title: Middleman のブログ投稿
+date: 2016/10/23
+category: HTML5
+---
+
+Hello World
